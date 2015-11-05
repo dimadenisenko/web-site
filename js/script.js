fd8af6c375e21c95307d10462a30fee3b8a15b30
Dmitry Denisenko <dmitryesten@gmail.com>
2015-11-05 13:31:47 +0600

    wrote main code for web-app

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 
-var wordFromInput = document.getElementById("tBoxFirstPage").onclick = iswordtrue();
+function isWordTrue(){
 
-function iswordtrue(){
-
+			var arrayTrueWords = ["пустыня","песок","караван","бархан"];
+			var trueWord = "пустыня";
+			var inputedWord = document.getElementById("tBoxFirstPage").value;
 	
+			console.log(inputedWord);
+
+			for(var i = 0; i < 4; i++){
+
+					if(arrayTrueWords[i] == inputedWord){
+
+						console.log("Слова совпадают");
+						tBoxForInputedWord.value='Слово '+ '"' +inputedWord+'"'+'верно';
+						break;
+						
+						}else{
+							console.log("Слова не совпадают");
+							tBoxForInputedWord.value='Слово '+ '"' +inputedWord+'"'+'неверно';
+						}
+
+				}
+					
+	}
 
 
 
-	
-}
+window.onload=function(){
+
+
+
+};
