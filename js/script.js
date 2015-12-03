2e4ff4a3dce2f5ff7c31833a1eea6dfa80cae4ee
Dmitry Denisenko <dmitryesten@gmail.com>
2015-12-03 11:54:04 +0600

    chaneged style of code

--- js/script.js
+++ js/script.js
@@ -1,41 +1,43 @@
 
 
 
-function isWordTrue(){
+function isWordTrue() {
     
-            $(document).ready(function(){
-                $('.materialboxed').materialbox();
-            });
-
-			var arrayTrueWords = ["пустыня","песок","караван","бархан"];
+            
+            var arrayTrueWords = [ "пустыня", "песок", "караван", "бархан"]; //array words
 			var inputedWord = document.getElementById("input-word").value;
+    
+            jQuery(document).ready(function () {
+                jQuery('.materialboxed').materialbox();
+            }) ;
 	
-			console.log(inputedWord);
+            
+            console.log(inputedWord);
 
-			for(var i = 0; i < 4; i++){
+			for (var iterate = 0; iterate < arrayTrueWords.length; iterate++) {
 
-					if(arrayTrueWords[i] == inputedWord){
-
-						console.log("Слова совпадают");
-                        disabledBox.value='Слово '+ '"' +inputedWord+'" '+'верно';
+					if ( arrayTrueWords[i] == inputedWord) {
+                            console.log("Слова совпадают");
                         
-                            if(inputedWord=="пустыня")
+                        disabledBox.value='Слово '+ '"' +inputedWord + '" ' + 'верно';
+                        
+                            if (inputedWord == "пустыня" )
                                 caseForDesert.value=inputedWord;
                         
-                            if(inputedWord=="песок")
-                                caseForSand.value=inputedWord;
+                            if (inputedWord =="песок")
+                                caseForSand.value = inputedWord;
                             
-                            if(inputedWord=="караван")
-                                caseForСaravan.value=inputedWord;
+                            if (inputedWord == "караван")
+                                caseForCaravan.value = inputedWord;
                         
                             if(inputedWord=="бархан")
-                                caseForDune.value=inputedWord;
+                                caseForDune.value = inputedWord;
                         
 						break;
 						
 						}else{
 							console.log("Слова не совпадают");
-							disabledBox.value='Слово '+ '"' +inputedWord+'"'+'неверно';
+							disabledBox.value='Слово ' + '"' +inputedWord+'"'+'неверно';
 						}
 
 				}
